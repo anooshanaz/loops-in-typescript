f9987f267062998aa4d52e660a6e375b56e1d7bf
Anoosha Naz <163389690+Anoosh-Naz@users.noreply.github.com>
2024-05-11 12:18:53 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,81 +1,82 @@
 // //loop
 // //for loop
 // //for(initialization; condition; increment/decrement){}
-// for(let i = 5; i <= 10; i++){
-//     console.log(i)
-// };
+for(let i = 5; i <= 10; i++){
+    console.log(i)
+};
 // //while loop
 // //while(condition){}
-// let i = 5
-// while(i <= 10) {
-//     console.log(i);
-//     i++;
-// };
+let i = 5
+while(i <= 10) {
+    console.log(i);
+    i++;
+};
 // //do while loop
 // //do {
 // //} while ();
-// let i = 5;
-// do{
-//     console.log(i);
-//     i++;
-// } while(i <= 10);
+let i = 5;
+do{
+    console.log(i);
+    i++;
+} while(i <= 10);
 
 // //find evenn number 
 // //loop for statement
-// for(let i = 2; i <= 50; i+=2) {
-//     console.log(i);
-// };
+for(let i = 2; i <= 50; i+=2) {
+    console.log(i);
+};
 
 // //while loop
-// let i = 2;
-// while(i <= 50){
-//     console.log(i);
-//     i+=2;
-// };
+let i = 2;
+while(i <= 50){
+    console.log(i);
+    i+=2;
+};
 
 // //do while loop
-// let i = 2;
-// do{
-//     console.log(i);
-//     i+=2;
-// } while(i <= 50);
+let i = 2;
+do{
+    console.log(i);
+    i+=2;
+} while(i <= 50);
 
 // //odd number
-// for(let i = 1; i <= 50; i+=2) {
-//     console.log(i);
-// }
+for(let i = 1; i <= 50; i+=2) {
+    console.log(i);
+}
 
 // //while loop
-// let i = 1;
-// while(i <= 50) {
-//     console.log(i);
-//     i+=2;
-// }
+let i = 1;
+while(i <= 50) {
+    console.log(i);
+    i+=2;
+}
 
 // //do while loop
-// let i = 1;
-// do{
-//     console.log(i);
-//     i+=2;
-// } while(i <= 50);
+let i = 1;
+do{
+    console.log(i);
+    i+=2;
+} while(i <= 50);
 
 //tables
-//table of 2
-// for(let i=2; i<=20; i+=2) {
-//     console.log(i)
-// }
+table of 2
+for(let i=2; i<=20; i+=2) {
+    console.log(i)
+}
 // //table of 3
-// for(let i=3; i<=30; i+=3) {
-//     console.log(i)
-//};
-// function tables(num1:number,num2:number,num3:number): void{
-//     for(let i=num1; i<=num2; i+=num3) {
-//         console.log(i);
-//     }
-// };
-// tables(2,20,2);
-// tables(4,40,4);
-// tables(12,120,12)
+for(let i=3; i<=30; i+=3) {
+    console.log(i)
+};
+
+function tables(num1:number,num2:number,num3:number): void{
+    for(let i=num1; i<=num2; i+=num3) {
+        console.log(i);
+    }
+};
+tables(2,20,2);
+tables(4,40,4);
+tables(12,120,12)
 
  let numbers:number[] = [1,2,3,4,5,6,7,8,9,10];
 for(let i=1; i<=numbers.length; i++){
@@ -83,38 +84,41 @@ for(let i=1; i<=numbers.length; i++){
  };
 
 //while loop
-// let i=0;
-// while(i<=10){
-//     console.log(i);
-//     i++
-// };
+let i=0;
+while(i<=10){
+    console.log(i);
+    i++
+};
 
 //do while loop
-// let i=0;
-// do{
-//     console.log(i);
-//     i++
-// }
-// while(i<=10)
-// let sum:number[] = [10,20,30,40,50]
-// console.log(sum.reverse())
+let i=0;
+do{
+    console.log(i);
+    i++
+}
+while(i<=10)
+
+let sum:number[] = [10,20,30,40,50]
+console.log(sum.reverse())
 
 //find max number using for loop
-// let arr:number[] = [60,70,80,90,100]
-// let max = arr[0]
-// for(let i=1; i<arr.length; i++){
-//     if(arr[i] > max) {
-//         max = arr[i]
-//     }
-//     }
-//     console.log(max)
-// let country:string[] = ["Pakistan","Iran","indonesia","Iraq","Turkey"]
-// let findcountry = "Iraq"
-// for(let i=0; i<=country.length; i++){
-//     if(country[i]===findcountry){
-//         console.log(`index of ${findcountry}: ${i}`)
-//     }
-// };
+let arr:number[] = [60,70,80,90,100]
+let max = arr[0]
+for(let i=1; i<arr.length; i++){
+    if(arr[i] > max) {
+        max = arr[i]
+    }
+    }
+    console.log(max)
+
+//find a string idex number using loop
+let country:string[] = ["Pakistan","Iran","indonesia","Iraq","Turkey"]
+let findcountry = "Iraq"
+for(let i=0; i<=country.length; i++){
+    if(country[i]===findcountry){
+        console.log(`index of ${findcountry}: ${i}`)
+    }
+};
 
 
 
